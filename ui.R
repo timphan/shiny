7045fba921ce7c71f9d884a8de01f183699f1770
timphan <phan.timothyv@gmail.com>
2014-07-10 17:20:33 -0700

    test

--- ui.R
+++ ui.R
@@ -1,29 +1,12 @@
 library(shiny)
-library(ggplot2)
-nba<- read.csv("http://timphan.github.io/nba.csv")
- 
- 
+
+# Define UI for miles per gallon application
 shinyUI(pageWithSidebar(
- 
-  headerPanel("NBA"),
-  
-  sidebarPanel(
- 
-    sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(nba),
-                value=min(1000, nrow(nba)), step=500, round=0),
-    
-    selectInput('x', 'X', names(nba)),
-    selectInput('y', 'Y', names(nba), names(nba)[[2]]),
-    selectInput('color', 'Color', c('None', names(dataset))),
-    
-    checkboxInput('jitter', 'Jitter'),
-    checkboxInput('smooth', 'Smooth'),
-    
-    selectInput('facet_row', 'Facet Row', c(None='.', names(nba))),
-    selectInput('facet_col', 'Facet Column', c(None='.', names(nba)))
-  ),
- 
-  mainPanel(
-    plotOutput('plot')
-  )
+
+  # Application title
+  headerPanel("Miles Per Gallon"),
+
+  sidebarPanel(),
+
+  mainPanel()
 ))
